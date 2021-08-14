0bd86050880148c8c427560c1d3ad13db77d8bdd
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-14 16:59:43 +0200

    Draft of the Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//components
+import Home from "./components/Home";
+
 function App() {
-    return <div>App</div>;
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
