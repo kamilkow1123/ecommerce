538931407cf7d173c964d7834a282a142b6e98d6
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-15 18:44:13 +0200

    Fetching posts for specific pages

--- src/components/products/ProductsList.tsx
+++ src/components/products/ProductsList.tsx
@@ -1,19 +1,24 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { fetchProducts } from "../../state/actions/products";
 //types
 import { State } from "../../state/reducers";
 import { IProduct } from "../../state/reducers/productsReducer";
 
 const ProductsList = () => {
+    const { page } = useParams<{ page: string }>();
     const dispatch = useDispatch();
     const products = useSelector(
         (state: State) => state.products.listOfProducts
     );
 
-    useEffect(() => {
-        dispatch(fetchProducts(1));
-    }, []);
+    useEffect(
+        () => {
+            dispatch(fetchProducts(page));
+        },
+        [ page ]
+    );
 
     return (
         <div>
--- src/state/actions/products.ts
+++ src/state/actions/products.ts
@@ -4,14 +4,14 @@ import { Dispatch } from "redux";
 import { ActionType } from "./types";
 import { Action } from ".";
 
-export const fetchProducts = (page: number) => async (
+export const fetchProducts = (page: string) => async (
     dispatch: Dispatch<Action>
 ) => {
-    if (!page) page = 1;
+    if (!page) page = "1";
 
     try {
         const response = await resultsAPI.get(`/product/list?page=${page}`);
-        console.log(response.data);
+        // console.log(response.data);
 
         dispatch({ type: ActionType.FETCH_PRODUCTS, payload: response.data });
     } catch (err) {
