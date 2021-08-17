0cb48c645c7ce716f85677748a303ab977acf325
kamilkow1123 <22kamil.kow@gmail.com>
2021-08-17 16:44:20 +0200

    Login form

--- src/components/accounts/LoginForm.tsx
+++ src/components/accounts/LoginForm.tsx
@@ -1,8 +1,74 @@
-const LoginForm = () => {
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+type Props = {
+    Login: (details: { username: string; password: string }) => void;
+    error: string;
+};
+
+const LoginForm: React.FC<Props> = ({ Login, error }) => {
+    const [ details, setDetails ] = useState<{
+        username: string;
+        password: string;
+    }>({ username: "", password: "" });
+
+    const submitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        Login(details);
+    };
+
     return (
-        <div>
-            <h1>LoginForm</h1>
-        </div>
+        <form onSubmit={submitHandler} className="form">
+            <h2>Login</h2>
+            {error !== "" ? (
+                <div className="form__error-login">{error}</div>
+            ) : (
+                ""
+            )}
+            <div className="form__group">
+                <label className="form__group__label" htmlFor="form-username">
+                    Username
+                </label>
+                <input
+                    className="form__group__input"
+                    type="text"
+                    name="username"
+                    id="username"
+                    onChange={e =>
+                        setDetails({ ...details, username: e.target.value })}
+                    value={details.username}
+                />
+            </div>
+            <div className="form__group">
+                <label className="form__group__label" htmlFor="password">
+                    Password:
+                </label>
+                <input
+                    className="form__group__input"
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={e =>
+                        setDetails({
+                            ...details,
+                            password: e.target.value,
+                        })}
+                    value={details.password}
+                />
+            </div>
+            <input
+                className="form__submit-button"
+                type="submit"
+                value="LOGIN"
+            />
+            <p>
+                Don't have an account?
+                <Link className="form__signup-button" to="/signup">
+                    Sign Up
+                </Link>
+            </p>
+        </form>
     );
 };
 
